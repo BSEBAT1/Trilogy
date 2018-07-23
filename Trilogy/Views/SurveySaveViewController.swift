7803a2fefe1a4d3c91e5af805a9dad6e1d3ea1b7
Berkay <Berkay_Sebat@yahoo.com>
2018-07-23 16:19:29 -0400

    added date picker

---
+++ Trilogy/Views/SurveySaveViewController.swift
@@ -0,0 +1,35 @@
+//
+//  SurveySaveViewController.swift
+//  Trilogy
+//
+//  Created by macbook pro on 7/23/18.
+//  Copyright © 2018 Trilogy. All rights reserved.
+//
+
+import UIKit
+
+class SurveySaveViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
